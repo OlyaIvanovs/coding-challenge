5c0138bf238f0b9fd539734673ca1c14a2383def
olya <olya@ivanovs.info>
2021-08-10 19:26:15 +1200

    Value is Decimal.

--- metrics.py
+++ metrics.py
@@ -3,6 +3,7 @@ Revenue, Expenses ,Gross Profit Margin, Net Profit Margin, Working Capital Ratio
 Assume all data from data.json are valid, so there is no need to validate it.
 """
 import json
+from decimal import Decimal
 
 # To avoid typos when check field value
 CATEGORY_REVENUE = "revenue"
@@ -20,20 +21,20 @@ def get_values(transactions):
     """Returns revenue, expenses, profit, assets and liability."""
     values = {"revenue": 0, "expenses": 0, "profit": 0, "assets": 0, "liability": 0}
     for t in transactions:
-        value = t["total_value"]
+        value = Decimal(t["total_value"])
         value_type = t["value_type"]
         category = t["account_category"]
         type = t["account_type"]
-        # Add up total revenue
+        # Total revenue
         if category == CATEGORY_REVENUE:
             values["revenue"] += value
-        # Add up total expenses
+        # Total expenses
         elif category == CATEGORY_EXPENSE:
             values["expenses"] += value
-        # Add up gross profit
+        # Gross profit
         elif type == TYPE_SALES and value_type == VALUE_DEBIT:
             values["profit"] += value
-        # Add up total assets
+        # Total assets
         elif (
             category == CATEGORY_ASSETS
             and value_type == VALUE_DEBIT
@@ -46,7 +47,7 @@ def get_values(transactions):
             and type in TYPES_ASSETS
         ):
             values["assets"] -= value
-        # Add up total liability
+        # Total liability
         elif (
             category == CATEGORY_LIABILITY
             and value_type == VALUE_CREDIT
@@ -73,5 +74,4 @@ if __name__ == "__main__":
         data = json.load(json_file).get("data")
 
     values = get_values(data)
-    print(values)
     metrics = calculate_metrics(values)
